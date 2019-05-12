ce4d5c11a46e9ab6e252d6f2502ff1ea475c64f5
Petru Birzu <petru.birzu96@gmail.com>
2019-05-12 15:51:58 +0200

    done img component with cache & loading

--- src/components/ImageX.tsx
+++ src/components/ImageX.tsx
@@ -1,28 +1,141 @@
-import React, { FunctionComponent, Component } from 'react';
-import { CachedImageProps, CustomCachedImage } from 'react-native-img-cache';
-import Image from 'react-native-image-progress';
-import { StyleSheet } from 'react-native';
+import React, { FunctionComponent, useState, useEffect, useRef } from 'react';
+import {
+  Image,
+  View,
+  ImageProps,
+  StyleSheet,
+  ViewStyle,
+  ActivityIndicator,
+  ImageURISource,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
-export interface ImageXProps extends CachedImageProps {
-  component?: new () => Component<any, any>;
+import { useLoading } from '../hooks';
+import { CachedImage } from 'react-native-img-cache';
+
+let defaultImgSource: ImageURISource;
+Icon.getImageSource('image', 200).then(src => {
+  defaultImgSource = src;
+});
+
+const getBaseDefaultImageProps = (): ImageProps => {
+  return {
+    source: defaultImgSource,
+    resizeMode: 'contain',
+  };
+};
+
+export interface ImageXProps extends ImageProps {
+  defaultImageProps?: ImageProps;
+  isDefaultImg?: boolean;
 }
 export const ImageX: FunctionComponent<ImageXProps> = ({
-  component = Image,
   style,
+  defaultImageProps: { style: defaultImgStyle, ...defaultImageProps } = {
+    style: {},
+  },
+  isDefaultImg = false,
+  onLoadStart,
+  onLoad,
+  onError,
   ...props
 }) => {
+  const [failed, setFailed] = useState(false);
+  const [showSpinner, setShowSpinner] = useLoading(false);
+  // hold ref to showSpinner for use in timeout
+  const showSpinnerRef = useRef(showSpinner);
+  showSpinnerRef.current = showSpinner;
+
+  useEffect(() => {
+    setShowSpinner(true);
+
+    const failTimeout = window.setTimeout(() => {
+      setFailed((prevFailed: boolean) => {
+        if (!prevFailed && showSpinnerRef.current) return true;
+        return prevFailed;
+      });
+    }, 5000);
+
+    return () => {
+      if (failTimeout) clearTimeout(failTimeout);
+    };
+  }, []);
+
+  let containerDimensions: ViewStyle = {};
+  if (style) {
+    const styleObj = StyleSheet.flatten([style]);
+    if (styleObj.width) containerDimensions.width = styleObj.width;
+    if (styleObj.height) containerDimensions.height = styleObj.height;
+  }
+
   return (
-    <CustomCachedImage
-      style={[styles.img, style]}
-      component={component}
-      {...props}
-    />
+    <View style={[styles.container, containerDimensions]}>
+      {failed || isDefaultImg ? (
+        <Image
+          style={[styles.img, styles.defaultImgBase, defaultImgStyle]}
+          onLoadStart={(...args) => {
+            setShowSpinner(true);
+            if (onLoadStart) onLoadStart(...args);
+          }}
+          onLoad={(...args) => {
+            setShowSpinner(false);
+            if (onLoad) onLoad(...args);
+          }}
+          onError={(...args) => {
+            setShowSpinner(false);
+            setFailed(true);
+            if (onError) onError(...args);
+          }}
+          {...props}
+          {...getBaseDefaultImageProps()}
+          {...defaultImageProps}
+        />
+      ) : (
+        <CachedImage
+          style={[styles.img, style]}
+          onLoadStart={(...args) => {
+            setShowSpinner(true);
+            if (onLoadStart) onLoadStart(...args);
+          }}
+          onLoad={(...args) => {
+            setShowSpinner(false);
+            if (onLoad) onLoad(...args);
+          }}
+          onError={(...args) => {
+            setShowSpinner(false);
+            setFailed(true);
+            if (onError) onError(...args);
+          }}
+          {...props}
+        />
+      )}
+
+      {showSpinner && (
+        <View style={styles.spinnerView}>
+          <ActivityIndicator />
+        </View>
+      )}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   img: {
     width: 140,
     height: 140,
+  },
+  spinnerView: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  defaultImgBase: {
+    overflow: 'visible',
   },
 });
