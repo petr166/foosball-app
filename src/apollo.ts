bf6399ff9b3f62ccdaa94e9e9defe4fe1c4db5c7
Petru Birzu <petru.birzu96@gmail.com>
2019-05-13 13:19:30 +0200

    added graphql error middleware

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,29 +1,47 @@
-import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost';
-import { setContext } from 'apollo-link-context';
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+  from,
+} from 'apollo-boost';
+import { onError } from 'apollo-link-error';
 import { getGlobal } from 'reactn';
 
 import { IGlobalState } from './global';
 import { API_URI } from './config/constants';
+import { logout } from './login';
 
 const httpLink = new HttpLink({
   uri: API_URI,
 });
 
-const authLink = setContext((_, { headers }) => {
+const authMiddleware = new ApolloLink((operation, forward) => {
   const authToken = getGlobal<IGlobalState>().authToken;
 
-  // return the headers to the context so httpLink can read them
-  return {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }: any) => ({
     headers: {
       ...headers,
       authorization: authToken ? authToken : '',
     },
-  };
+  }));
+
+  return forward ? forward(operation) : null;
+});
+
+const errorMiddleware = onError((error: any) => {
+  console.log('====================================');
+  console.log('GraphQL error:', error);
+  console.log('====================================');
+
+  const { networkError = {} } = error;
+  if (networkError.statusCode === 401) logout();
 });
 
 export const initApolloClient = () => {
   return new ApolloClient({
-    link: authLink.concat(httpLink),
+    link: from([errorMiddleware, authMiddleware, httpLink]),
     cache: new InMemoryCache(),
   });
 };
