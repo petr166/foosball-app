549de69550d694e128c242f3d0211a4b46a08525
Petru Birzu <petru.birzu96@gmail.com>
2019-05-25 11:26:02 +0200

    refactored searchbar in ts

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -7,11 +7,107 @@ import {
   TouchableOpacity,
   LayoutAnimation,
   Platform,
+  TextInputProps,
 } from 'react-native';
-import pt from 'prop-types';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import { colors } from '../config/styles';
 import { TextX } from './TextX';
+
+export interface SearchBarProps {
+  onFocus?: () => void;
+  isActive?: boolean;
+  onCancelPress?: () => void;
+  onChangeText?: (val: string) => void;
+  inputProps?: TextInputProps;
+  value?: string;
+  placeholder?: string;
+  refSet?: (ref: any) => void;
+}
+class SearchBar extends React.Component<SearchBarProps, { isActive: boolean }> {
+  static defaultProps = {
+    placeholder: 'Search',
+    isActive: false,
+  };
+
+  textInput: any;
+
+  constructor(props: SearchBarProps) {
+    super(props);
+    this.state = {
+      isActive: !!props.isActive,
+    };
+  }
+
+  handleCancelPress = () => {
+    this.textInput.clear();
+    this.props.onChangeText && this.props.onChangeText('');
+
+    this.textInput.blur();
+    this.props.onCancelPress && this.props.onCancelPress();
+  };
+
+  setActive = (isActive = true) => {
+    if (this.state.isActive !== isActive) {
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+      this.setState({ isActive });
+    }
+  };
+
+  render() {
+    const {
+      onChangeText,
+      inputProps: { onFocus = null, onBlur = null, ...inputProps } = {},
+      placeholder,
+      refSet,
+    } = this.props;
+    const { isActive } = this.state;
+
+    return (
+      <SafeAreaView>
+        <View style={styles.headerContainer}>
+          <View style={styles.searchIcon}>
+            <Icon name="search" size={18} color="#808080" />
+          </View>
+
+          <TextInput
+            ref={textInput => {
+              refSet && refSet(textInput);
+              this.textInput = textInput;
+            }}
+            style={[styles.input, isActive && styles.inputActive]}
+            placeholder={placeholder}
+            autoCapitalize="none"
+            autoCorrect={false}
+            spellCheck={false}
+            clearButtonMode="while-editing"
+            maxLength={30}
+            selectionColor="#000" // cursor
+            onFocus={(...args) => {
+              onFocus && onFocus(...args);
+              this.setActive();
+            }}
+            onBlur={(...args) => {
+              onBlur && onBlur(...args);
+              this.setActive(false);
+            }}
+            onChangeText={onChangeText}
+            underlineColorAndroid="rgba(0,0,0,0)"
+            returnKeyType="search"
+            placeholderTextColor="#808080"
+            {...inputProps}
+          />
+
+          {isActive && (
+            <TouchableOpacity onPress={this.handleCancelPress}>
+              <TextX style={styles.cancelText} shadowed={false}>
+                Cancel
+              </TextX>
+            </TouchableOpacity>
+          )}
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   headerContainer: {
@@ -48,101 +144,5 @@ const styles = StyleSheet.create({
     zIndex: 1,
   },
 });
-
-class SearchBar extends React.Component {
-  static defaultProps = {
-    placeholder: 'Search',
-    isActive: false,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isActive: !!props.isActive,
-    };
-  }
-
-  handleCancelPress = () => {
-    this.textInput.clear();
-    this.props.onChangeText && this.props.onChangeText('');
-
-    this.textInput.blur();
-    this.props.onCancelPress && this.props.onCancelPress();
-  };
-
-  setActive = (isActive = true) => {
-    if (this.state.isActive !== isActive) {
-      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-      this.setState({ isActive });
-    }
-  };
-
-  render() {
-    const {
-      onChangeText = null,
-      inputProps: { onFocus, onBlur, ...inputProps } = {},
-      placeholder,
-      refSet,
-    } = this.props;
-    const { isActive } = this.state;
-
-    return (
-      <SafeAreaView>
-        <View style={styles.headerContainer}>
-          <View style={styles.searchIcon}>
-            <Icon name="search" size={18} color="#808080" />
-          </View>
-
-          <TextInput
-            ref={textInput => {
-              refSet && refSet(textInput);
-              this.textInput = textInput;
-            }}
-            style={[styles.input, isActive && styles.inputActive]}
-            placeholder={placeholder}
-            autoCapitalize="none"
-            autoCorrect={false}
-            spellCheck={false}
-            clearButtonMode="while-editing"
-            maxLength={30}
-            selectionColor="#000" // cursor
-            onFocus={() => {
-              onFocus && onFocus();
-              this.setActive();
-            }}
-            onBlur={() => {
-              onBlur && onBlur();
-              this.setActive(false);
-            }}
-            onChangeText={onChangeText}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            returnKeyType="search"
-            placeholderTextColor="#808080"
-            {...inputProps}
-          />
-
-          {isActive && (
-            <TouchableOpacity onPress={this.handleCancelPress}>
-              <TextX style={styles.cancelText} shadowed={false}>
-                Cancel
-              </TextX>
-            </TouchableOpacity>
-          )}
-        </View>
-      </SafeAreaView>
-    );
-  }
-}
-
-SearchBar.propTypes = {
-  onFocus: pt.func,
-  isActive: pt.bool,
-  onCancelPress: pt.func,
-  onChangeText: pt.func,
-  inputProps: pt.shape({}),
-  value: pt.string,
-  placeholder: pt.string,
-  refSet: pt.func,
-};
 
 export default SearchBar;
