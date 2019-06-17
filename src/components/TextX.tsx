f36823f38fa91ae459f80c47763bfd121f56e12e
Petru Birzu <petru.birzu96@gmail.com>
2019-06-17 17:38:26 +0200

    added roboto font

--- src/components/TextX.tsx
+++ src/components/TextX.tsx
@@ -1,6 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import { Text, TextProps, StyleSheet } from 'react-native';
 
+const fontFamilyMap = {
+  regular: 'Roboto_Regular',
+  medium: 'Roboto_Medium',
+  bold: 'Roboto_Bold',
+  black: 'Roboto_Black',
+};
+
 export interface TextXProps extends TextProps {
   shadowed?: boolean;
 }
@@ -9,9 +16,23 @@ export const TextX: FunctionComponent<TextXProps> = ({
   shadowed = true,
   ...props
 }) => {
+  let fontFamily = fontFamilyMap.medium;
+  if (style) {
+    const propStyles = StyleSheet.flatten(style);
+    if (propStyles.fontWeight) {
+      const numberWeight = Number(propStyles.fontWeight);
+      if (numberWeight) {
+        if (numberWeight <= 400) fontFamily = fontFamilyMap.regular;
+        else if (numberWeight <= 500) fontFamily = fontFamilyMap.medium;
+        else if (numberWeight <= 700) fontFamily = fontFamilyMap.bold;
+        else if (numberWeight <= 900) fontFamily = fontFamilyMap.black;
+      }
+    }
+  }
+
   return (
     <Text
-      style={[styles.text, shadowed && styles.shadowed, style]}
+      style={[styles.text, shadowed && styles.shadowed, style, { fontFamily }]}
       {...props}
     />
   );
@@ -20,7 +41,7 @@ export const TextX: FunctionComponent<TextXProps> = ({
 const styles = StyleSheet.create({
   text: {
     color: '#000',
-    fontWeight: '500',
+    fontFamily: fontFamilyMap.medium,
     fontSize: 15,
   },
   shadowed: {
